5b55dc0a24a7081050bfcbcbf07aa168b1338c5c
Daniel Williams <danwill89@hotmail.com>
2023-10-18 11:53:36 +0100

    feat: added second card

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from 'next/image'
-import styles from './page.module.css'
 import Card from './components/card'
 
 export default function Home() {
   return (
-    <Card name="John Doe" />
+    <>
+      <Card name="John Doe" />
+      <Card name="Jane Doe" />
+    </>
   )
 }
