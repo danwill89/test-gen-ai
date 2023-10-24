20e80827c8ca5cab537968860ce6a185903fc22a
Daniel Williams <danwill89@hotmail.com>
2023-10-24 14:33:35 +0100

    feat: Job Added

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -1,9 +1,10 @@
 import styles from '../styles/card.module.scss';
-const Card = ({ name, age }: { name: string, age: number }) => {
+const Card = ({ name, age, job }: { name: string, age: number, job: string }) => {
     return (
         <section className={styles.card}>
             <p>{name}</p>
             <p>{age}</p>
+            <p>{job}</p>
         </section>
     )
 }
