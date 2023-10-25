6c6411f793f5d1f205eac3d1922d971dd3f9afe5
danwill89 <59125712+danwill89@users.noreply.github.com>
2023-10-25 14:35:11 +0100

    feat: Job Added (#5)
    * feat: Job Added
    
    * Update card.tsx
    
    * added fixes
    
    * Update main.yml
    
    * Update main.yml
    
    * Update pr_agent.yml

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -1,9 +1,22 @@
 import styles from '../styles/card.module.scss';
-const Card = ({ name, age }: { name: string, age: number }) => {
+
+export enum Job {
+    SOFTWARE_ENGINEER = 'Software Engineer',
+    DATA_SCIENTIST = 'Data Scientist',
+    PRODUCT_MANAGER = 'Product Manager',
+}
+interface CardProps {
+    name: string;
+    age: number;
+    job: Job;
+}
+
+const Card = ({ name, age, job }: CardProps) => {
     return (
         <section className={styles.card}>
             <p>{name}</p>
             <p>{age}</p>
+            <p>{job}</p>
         </section>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import Card from './components/card'
+import Card, { Job } from './components/card'
 
 export default function Home() {
   return (
     <>
-      <Card name="John Doe" age={30} />
-      <Card name="Jane Doe" age={25} />
-      <Card name="Bob Doe" age={20} />
+      <Card name="John Doe" age={30} job={Job.SOFTWARE_ENGINEER}/>
+      <Card name="Jane Doe" age={25} job={Job.SOFTWARE_ENGINEER}/>
+      <Card name="Bob Doe" age={20} job={Job.SOFTWARE_ENGINEER}/>
     </>
   )
 }
