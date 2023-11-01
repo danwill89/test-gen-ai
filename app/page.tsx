45a86e79329c6abb6b409950c9f4c6b4d0aa2325
danwill89 <59125712+danwill89@users.noreply.github.com>
2023-11-01 14:16:43 +0000

    Added title prop to Card component (#6)
    * title added
    
    * Update card.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ import Card, { Job } from './components/card'
 export default function Home() {
   return (
     <>
-      <Card name="John Doe" age={30} job={Job.SOFTWARE_ENGINEER}/>
-      <Card name="Jane Doe" age={25} job={Job.SOFTWARE_ENGINEER}/>
-      <Card name="Bob Doe" age={20} job={Job.SOFTWARE_ENGINEER}/>
+      <Card name="John Doe" age={30} job={Job.SOFTWARE_ENGINEER} title="Senior"/>
+      <Card name="Jane Doe" age={25} job={Job.SOFTWARE_ENGINEER} title='Associate'/>
+      <Card name="Bob Doe" age={20} job={Job.SOFTWARE_ENGINEER} title='Intern'/>
     </>
   )
 }
